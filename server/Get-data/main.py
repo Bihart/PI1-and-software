f90de01152f985ca0da1b76e3fde4184fc926d73
Bihart <sanket.bihari@gmail.com>
2021-10-09 00:01:52 -0500

    Init Script to scraping

---
+++ server/Get-data/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+CLI-tool or little program to get data of the web
+"""
+import sys
+
+
+def main():
+    """Main function"""
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
