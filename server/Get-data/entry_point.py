ce6067abf6000a7a0e595b00e6e3af583656549f
jsantac6 <jsantac6@eafit.edu.co>
2021-10-17 22:31:52 -0500

    5 horas aiuda

---
+++ server/Get-data/entry_point.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
+import pandas as pd
+
+
+app = Flask(__name__)
+api = Api(app)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+class HelloWorld(Resource):
+    json = pd.read_json('outj.json')
+    to_json = json.to_dict()
+    @cross_origin()
+    def get(self):
+        response=jsonify(self.to_json)
+        #return {'hello': 'world'}
+        #response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+api.add_resource(HelloWorld, '/api')
+
+def main():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
--- server/Get-data/main.py
+++ server/Get-data/main.py
@@ -3,12 +3,20 @@
 CLI-tool or little program to get data of the web
 """
 import sys
+from flask import Flask
+from flask_restful import Resource, Api
 
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
 
 def main():
-    """Main function"""
-    sys.exit(0)
-
+    app.run(debug=True)
 
 if __name__ == '__main__':
     main()
