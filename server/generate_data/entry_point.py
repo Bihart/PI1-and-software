d773f677f65112425c2378d8947e12725f2a63d8
jsantac6 <jsantac6@eafit.edu.co>
2021-10-31 18:35:44 -0500

    post sufrido, pero gucci, putos imports, puto git

--- server/generate_data/entry_point.py
+++ server/generate_data/entry_point.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_cors import CORS, cross_origin
 import pandas as pd
-
+import variables
 
 app = Flask(__name__)
 api = Api(app)
@@ -20,6 +20,12 @@ class SensorApi(Resource):
         response=jsonify(self.to_json)
         return response
 
+    def post(self):
+        json_variables = request.get_json(force = True)
+        valor_area = json_variables["area"]
+        valor_presupuesto = json_variables["presupuesto"]
+
+        return variables.presupuesto_area(valor_area, valor_presupuesto)
 api.add_resource(SensorApi, '/api')
 
 def main():
