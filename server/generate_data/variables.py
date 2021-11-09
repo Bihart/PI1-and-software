7bdebc1c546abee432427c788babbd57e99d3f19
Bihart <sanket.bihari@gmail.com>
2021-11-09 15:52:56 -0500

    UPDATE generate-data to API

--- server/generate_data/variables.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-def presupuesto_area(area, presupuesto):
-
-    sub_area = area / 0.3
-    sub_presupuesto = presupuesto / sub_area
-    return f"su presupuesto por area es {sub_presupuesto}"
