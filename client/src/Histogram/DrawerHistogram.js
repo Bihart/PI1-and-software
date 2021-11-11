35822fab02b7c3c16b125eb76e249f579a46d0ed
Bihart <sanket.bihari@gmail.com>
2021-11-11 17:36:17 -0500

    📊 FIX animation on X Axis

---
+++ client/src/Histogram/DrawerHistogram.js
@@ -0,0 +1,93 @@
+import * as d3 from 'd3';
+
+function setup(reference, data)
+{
+  const margin = { top: 40, right: 30, bottom: 60, left: 40},
+        width = 720 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+  const svg = d3.select(reference.current)
+                .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform",
+                      `translate(${margin.left},${margin.top})`);
+
+  // x axis: initilization
+  const x = d3.scaleLinear()
+              .range([0, width]);
+
+  const xAxis = svg.append("g")
+                   .attr("transform", `translate(0, ${height + 1})`)
+
+  // y axis: initilization
+  const y = d3.scaleLinear()
+              .range([height, 0]);
+
+  const yAxis = svg.append("g");
+
+  svg.append("text")
+     .attr("class", "x label")
+     .attr("text-anchor", "end")
+     .attr("x", width / 2)
+     .attr("y", height + 40)
+     .text("Precio de sensores");
+
+  svg.append("text")
+     .attr("class", "title label")
+     .attr("text-anchor", "end")
+     .attr("x", width / 2)
+     .attr("y", - 10)
+     .text("Histograma");
+
+  svg.append("text")
+     .attr("class", "y label")
+     .attr("text-anchor", "end")
+     .attr("y", - 39)
+     .attr("dy", ".75em")
+     .attr("transform", "rotate(-90)")
+     .text("Número de sensores");
+
+  return [svg, height, x, y, yAxis, xAxis];
+}
+
+function update(data, nBin, [svg, height, x, y, yAxis, xAxis])
+{
+  const max = Math.max(...data) + 10;
+  const min = Math.min(...data) - 10;
+
+  const histogram = d3.bin()
+                      .value((d) => d)
+                      .domain(x.domain())
+                      .thresholds(x.ticks(nBin));
+
+  const bins = histogram(data);
+
+  x.domain([min, max])
+  y.domain([0, d3.max(bins, function(d) { return d.length  })]);
+
+  yAxis
+    .transition()
+    .duration(1000)
+    .call(d3.axisLeft(y));
+
+  xAxis
+    .transition()
+    .duration(1000)
+    .call(d3.axisBottom(x));
+
+  const u = svg.selectAll("rect")
+               .data(bins);
+
+  u.join("rect")
+   .transition()
+   .duration(1000)
+   .attr("x", 1)
+   .attr("transform", function(d) { return `translate(${x(d.x0)}, ${y(d.length)})`})
+   .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+   .attr("height", function(d) { return height - y(d.length); })
+   .style("fill", "#69b3a2")
+}
+
+export { setup, update };
--- client/src/Histogram/Histogram.jsx
+++ client/src/Histogram/Histogram.jsx
@@ -1,89 +1,16 @@
 import React, { Fragment, useRef, useEffect, useState } from 'react';
-import * as d3 from 'd3';
+import { setup, update } from './DrawerHistogram'
 
-function setup(reference, data)
-{
-  let max = d3.max(data) + 10;
-  let min = d3.min(data) - 10;
+function useNBins(numberBins) {
+  const [nBins, setnBins ] = useState(numberBins);
 
-  const margin = { top: 40, right: 30, bottom: 60, left: 40},
-        width = 720 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+  const changeNumberBins = (newNumberBins) => {
+    newNumberBins > 100 || newNumberBins < 1 ?
+      setnBins(20) :
+      setnBins(newNumberBins);
+  }
 
-  const svg = d3.select(reference.current)
-                .append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform",
-                      `translate(${margin.left},${margin.top})`);
-
-  // x axis: scale and draw
-  const x = d3.scaleLinear()
-              .domain([min, max])
-              .range([0, width]);
-  svg.append("g")
-     .attr("transform", `translate(0, ${height})`)
-     .call(d3.axisBottom(x));
-
-  svg.append("text")
-     .attr("class", "x label")
-     .attr("text-anchor", "end")
-     .attr("x", width / 2)
-     .attr("y", height + 40)
-     .text("Precio de sensores");
-
-  svg.append("text")
-     .attr("class", "title label")
-     .attr("text-anchor", "end")
-     .attr("x", width / 2)
-     .attr("y", - 10)
-     .text("Histograma");
-
-  svg.append("text")
-     .attr("class", "y label")
-     .attr("text-anchor", "end")
-     .attr("y", - 39)
-     .attr("dy", ".75em")
-     .attr("transform", "rotate(-90)")
-     .text("Número de sensores");
-
-  // y axis: initilization
-  const y = d3.scaleLinear()
-              .range([height, 0]);
-
-  const yAxis = svg.append("g");
-
-  return [svg, height, x, y, yAxis];
-}
-
-function update(data, nBin, [svg, height, x, y, yAxis])
-{
-    const histogram = d3.bin()
-                        .value((d) => d)
-                        .domain(x.domain())
-                        .thresholds(x.ticks(nBin));
-
-    const bins = histogram(data);
-
-    y.domain([0, d3.max(bins, function(d) { return d.length  })]);
-
-    yAxis
-      .transition()
-      .duration(1000)
-      .call(d3.axisLeft(y));
-
-    const u = svg.selectAll("rect")
-                 .data(bins);
-
-    u.join("rect")
-     .transition()
-     .duration(1000)
-     .attr("x", 1)
-     .attr("transform", function(d) { return `translate(${x(d.x0)}, ${y(d.length)})`})
-     .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-     .attr("height", function(d) { return height - y(d.length); })
-     .style("fill", "#69b3a2")
+  return [nBins, changeNumberBins]
 }
 
 function Histogram({data}) {
@@ -91,7 +18,7 @@ function Histogram({data}) {
   const plot1 = useRef(null);
   const needles = useRef([]);
 
-  const [nBins, setnBins ] = useState(20);
+  const [nBins, setNBins ] = useNBins(20);
 
   useEffect(() => {
     needles.current  = setup(plot1, data);
@@ -103,12 +30,8 @@ function Histogram({data}) {
   }, [data, nBins]);
 
   const handleOnChange = (e) => {
-    const newValue = e.target.value;
-    if(newValue > 100 || newValue < 1){
-      setnBins(20);
-    } else {
-      setnBins(newValue);
-    }
+    const newNumberOfBinds = e.target.value;
+    setNBins(newNumberOfBinds)
   }
 
   return (
